56b0795b5f68720ed69f4847f8c89458a29196cc
unknown <ulyamba@gmail.com>
2023-05-14 18:49:27 +0200

    DZ JS 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryContainer = document.querySelector(`.gallery`);
+const elementsGallery = createElementGallery(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML(`beforeend`, elementsGallery);
+
+function createElementGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"        
+        alt="${description}"
+      />
+    </a>
+  </li >
+    `;
+    })
+    .join('');
+}
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
